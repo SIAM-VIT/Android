1861be02d3560f6d29dd4570d049a2ca2a513afc
HrithikMittal <adhikanshmittal@gmail.com>
2019-03-30 13:44:18 +0530

    new UI layout

--- 3.PuppyAge/app/src/main/java/com/example/puppyage/MainActivity.java
+++ 3.PuppyAge/app/src/main/java/com/example/puppyage/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.master_layout);
+        setContentView(R.layout.test_layout);
     }
 
 //    public void changeImg(View viewChange){
