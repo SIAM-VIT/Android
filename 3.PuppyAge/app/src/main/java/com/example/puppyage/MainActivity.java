3ef479e2a603a7d0d77524b5a9519f58cb17b9b7
HrithikMittal <adhikanshmittal@gmail.com>
2019-03-30 14:58:26 +0530

    app done

--- 3.PuppyAge/app/src/main/java/com/example/puppyage/MainActivity.java
+++ 3.PuppyAge/app/src/main/java/com/example/puppyage/MainActivity.java
@@ -2,19 +2,36 @@ package com.example.puppyage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_layout);
+        setContentView(R.layout.main_layout);
     }
 
 //    public void changeImg(View viewChange){
 //        ImageView img1 = (ImageView)findViewById(R.id.image1);
 //        img1.setImageResource(R.drawable.lighthouse);
 //    }
+
+    public void calAge(View view){
+        EditText year = (EditText)findViewById(R.id.year);
+        int yearOfBorn = Integer.parseInt(year.getText().toString());
+        Log.i("Year",year.getText() + "");
+        int age = 2019 - yearOfBorn;
+        Toast.makeText(getApplicationContext(),"Age is"+age,Toast.LENGTH_SHORT);
+        Log.i("Age",age + "");
+
+        TextView showAge = (TextView)findViewById(R.id.age);
+        showAge.setText(age+"");
+    }
+
 }
